3d98dcc43d262d3c66b0f430993b7a5dbc518c74
Florin D <doarzic@gmail.com>
2019-02-28 22:20:59 +0200

    Initial commit - first use of Git

---
+++ 002_ifelse_RewriteIf.js
@@ -0,0 +1,8 @@
+// Rewrite this if using the ternary operator '?':
+// if (a + b < 4) {
+// / result = "Below";
+// } else {
+//   result = "Over";
+// }
+
+result = (a + b < 4) ? "Below" : "Over";
---
+++ 003_ifelse_RewriteIfElse.js
@@ -0,0 +1,16 @@
+// Rewrite if..else using multiple ternary operators '?'.
+// let message;
+// if (login == "Employee") {
+//   message = "hello";
+// } else if (login == "Director") {
+//   message = "Greetings";
+// } else if (login == "") {
+//   message ="No login";
+// } else {
+//   message = "";
+// }
+
+let message = (login == "Employee") ? "Hello" :
+  (login == "Director") ? "Greetings" :
+  (login = "") ? "No login" :
+  "";
